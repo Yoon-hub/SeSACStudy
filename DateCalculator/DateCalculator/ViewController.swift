8f0f768ed2fd5bb1f12d79ef0567f6458d20403f
Yoon-hub <kong4170@naver.com>
2022-07-14 20:49:43 +0900

    add DateCalculator

--- DateCalculator/DateCalculator/ViewController.swift
+++ DateCalculator/DateCalculator/ViewController.swift
@@ -8,12 +8,61 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var imageCollection: [UIImageView]!
+    @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet weak var secondLabel: UILabel!
+    @IBOutlet weak var thirdLabel: UILabel!
+    @IBOutlet weak var fourthLabel: UILabel!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    var labelArr : [UILabel] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        for i in imageCollection{
+            i.layer.cornerRadius = 25
+        }
+        updateDate()
+        
+    
+        
+        
+        
+        //DateFormatter: 알아보기 쉬운 날짜 + 시간대 (yyyy MM dd hh:mm:ss)
+//        let format = DateFormatter()
+//        format.dateFormat = "M월 d일, yy년"  // format을 이형태로 하겠다
+//
+//        let result = format.string(from: Date())
+//
+//        let word = "3월 2일, 19년"
+//        let dateResult = format.date(from: word)
     }
-
-
+    
+    func updateDate() {
+        
+        let format = DateFormatter()
+        format.locale = Locale(identifier: "ko_KR")  // 한국시간으로 변경
+        format.timeZone = TimeZone(abbreviation: "KST") // 한국시간으로 변경
+        format.dateFormat = "yyyy년\n MM월 d일"
+        
+        let today = datePicker.date
+        let day100 = today.addingTimeInterval(86400 * 100)
+        let day200 = today.addingTimeInterval(86400 * 200)
+        let day300 = today.addingTimeInterval(86400 * 300)
+        let day400 = today.addingTimeInterval(86400 * 400)
+        
+        firstLabel.text = format.string(from: day100)
+        secondLabel.text = format.string(from: day200)
+        thirdLabel.text = format.string(from: day300)
+        fourthLabel.text = format.string(from: day400)
+  
+    }
+    
+    @IBAction func selectDate(_ sender: UIDatePicker) {
+        updateDate()
+    }
+    
 }
 
---
+++ DateCalculator/README.md
@@ -0,0 +1,1 @@
+<img src = "https://user-images.githubusercontent.com/92036498/178975466-ee1658ca-8efc-423a-abee-aa2184bf2078.png" width = "190" height = "400"/>
